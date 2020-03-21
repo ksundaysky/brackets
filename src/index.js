34d84a33d264023d531a4a977130af023283ad5d
Krzysztof Niedzielski <krzysztof.niedzielski96@gmail.com>
2020-03-21 19:47:19 +0100

    Add solution to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  regexp = new RegExp(bracketsConfig.map(bracket => bracket.join('')).join(' ').replace(/[(){}|[\]]/g, '\\$&').replace(/\s/g, '|'), "g");
+  while (regexp.test(str)) {
+    str = str.replace(regexp, '');    
+  }
+  return !str;
 }
